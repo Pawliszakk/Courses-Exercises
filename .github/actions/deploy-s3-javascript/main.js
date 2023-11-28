769186bfd631ac95d142a92f252bf1adef73a14d
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-11-28 20:58:59 +0100

    added new workflows, deleted app

--- .github/actions/deploy-s3-javascript/main.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
-
-const run = () => {
-    core.notice('Hello from custom js action')
-};
-
-run();
