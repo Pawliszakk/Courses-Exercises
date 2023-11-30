cc21bf6272f957efed78d89d75ef93535fb229c8
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-11-30 20:08:10 +0100

    added tests for validation functions

--- front/src/util/validation.js
+++ front/src/util/validation.js
@@ -1,11 +1,15 @@
 export function validateStringNotEmpty(value) {
-  if (value.trim().length === 0) {
-    throw new Error('Invalid input - must not be empty.');
-  }
+	if (!value || value.trim().length === 0) {
+		throw new Error('Invalid input - must not be empty.');
+	}
 }
 
 export function validateNumber(number) {
-  if (isNaN(number)) {
-    throw new Error('Invalid number input.');
-  }
+	if (!number) {
+		throw new Error('No value is provided');
+	}
+
+	if (isNaN(number) || typeof number !== 'number') {
+		throw new Error('Invalid number input.');
+	}
 }
