08da31b82d31f4f7666c6456eec7625e3449ff98
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-12-03 00:34:14 +0100

    fixed code

--- front/src/math.js
+++ front/src/math.js
@@ -1,11 +1,22 @@
+import { cleanNumbers } from './util/numbers.js';
+
 export function add(numbers) {
 	let sum = 0;
 
 	for (const number of numbers) {
 		sum += +number;
 	}
-	if (isNaN(sum)) {
-		return NaN;
-	}
 	return sum;
+}
+
+export function calculateResult(numberValues) {
+	let result = '';
+	try {
+		const numbers = cleanNumbers(numberValues);
+
+		result = add(numbers).toString();
+	} catch (error) {
+		result = error.message;
+	}
+	return result;
 }
