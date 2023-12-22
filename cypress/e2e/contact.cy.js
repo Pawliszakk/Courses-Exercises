f68d7c6a51b1ab641baaff465fbf14423f75ec67
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-12-22 21:59:33 +0100

    added new tests

---
+++ cypress/e2e/contact.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress"/>
+
+describe('contact form', () => {
+	it('should submit the form', () => {
+		cy.visit('http://localhost:5173/about');
+		cy.get('[data-cy="contact-input-message"]').type('Test message');
+		cy.get('[data-cy="contact-input-name"]').type('Test name');
+		cy.get('[data-cy="contact-input-email"]').type('testemail@o2.pl');
+
+		cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+
+		cy.get('@submitBtn').should('not.be.disabled').contains('Send Message');
+
+		cy.get('@submitBtn').click();
+		cy.get('@submitBtn').should('be.disabled').contains('Sending...');
+	});
+});
