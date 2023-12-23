007b1269499a42408a7fd787ab06153ab8d1e798
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-12-23 22:09:48 +0100

    added blur, focus testing

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -1,17 +1,44 @@
 /// <reference types="Cypress"/>
 
 describe('contact form', () => {
-	it('should submit the form', () => {
+	// it('should submit the form', () => {
+	// 	cy.visit('http://localhost:5173/about');
+	// 	cy.get('[data-cy="contact-input-message"]').type('Test message');
+	// 	cy.get('[data-cy="contact-input-name"]').type('Test name');
+	// 	cy.get('[data-cy="contact-input-email"]').type('testemail@o2.pl{enter}');
+	// 	cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+	// 	cy.get('@submitBtn').should('not.be.disabled').contains('Send Message');
+
+	// 	cy.get('@submitBtn').click();
+	// 	cy.get('@submitBtn').should('be.disabled').contains('Sending...');
+	// });
+	it('should validate the form input', () => {
 		cy.visit('http://localhost:5173/about');
-		cy.get('[data-cy="contact-input-message"]').type('Test message');
-		cy.get('[data-cy="contact-input-name"]').type('Test name');
-		cy.get('[data-cy="contact-input-email"]').type('testemail@o2.pl');
-		//then
 		cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
-
-		cy.get('@submitBtn').should('not.be.disabled').contains('Send Message');
-
 		cy.get('@submitBtn').click();
-		cy.get('@submitBtn').should('be.disabled').contains('Sending...');
+		cy.get('@submitBtn').then((el) => {
+			expect(el).not.to.have.attr('disabled');
+			expect(el).not.to.equal('Sending...');
+		});
+		cy.get('[data-cy="contact-input-message"]').blur();
+		cy.get('[data-cy="contact-input-message"]')
+			.parent()
+			.should('have.attr', 'class')
+			.and('match', /invalid/);
+
+		cy.get('[data-cy="contact-input-name"]').focus().blur();
+		cy.get('[data-cy="contact-input-name"]')
+			.parent()
+			.should('have.attr', 'class')
+			.and('match', /invalid/);
+
+		cy.get('[data-cy="contact-input-email"]').focus().blur();
+		cy.get('[data-cy="contact-input-email"]')
+			.parent()
+			.should('have.attr', 'class')
+			.and('match', /invalid/);
+
+		// cy.get('[data-cy="contact-input-name"]').blur();
+		// cy.get('[data-cy="contact-input-email"]').blur();
 	});
 });
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,144 +2,144 @@ import { useEffect, useReducer, useState } from 'react';
 import classes from './ContactForm.module.css';
 
 const initialState = {
-  name: {
-    value: '',
-    blurred: false,
-  },
-  email: {
-    value: '',
-    blurred: false,
-  },
-  message: {
-    value: '',
-    blurred: false,
-  },
+	name: {
+		value: '',
+		blurred: false,
+	},
+	email: {
+		value: '',
+		blurred: false,
+	},
+	message: {
+		value: '',
+		blurred: false,
+	},
 };
 
 const formReducer = (state, action) => {
-  if (action.type === 'INPUT_CHANGE') {
-    return {
-      ...state,
-      [action.input]: {
-        value: action.value,
-        blurred: false,
-      },
-    };
-  }
+	if (action.type === 'INPUT_CHANGE') {
+		return {
+			...state,
+			[action.input]: {
+				value: action.value,
+				blurred: false,
+			},
+		};
+	}
 
-  if (action.type === 'INPUT_BLUR') {
-    return {
-      ...state,
-      [action.input]: {
-        ...state[action.input],
-        blurred: true,
-      },
-    };
-  }
+	if (action.type === 'INPUT_BLUR') {
+		return {
+			...state,
+			[action.input]: {
+				...state[action.input],
+				blurred: true,
+			},
+		};
+	}
 
-  return initialState;
+	return initialState;
 };
 
 function ContactForm() {
-  const [formState, dispatch] = useReducer(formReducer, initialState);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+	const [formState, dispatch] = useReducer(formReducer, initialState);
+	const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const { name, email, message } = formState;
-  const nameIsValid = name.value.trim() !== '';
-  const emailIsValid = email.value.trim() !== '' && email.value.includes('@');
-  const messageIsValid = message.value.trim() !== '';
+	const { name, email, message } = formState;
+	const nameIsValid = name.value.trim() !== '';
+	const emailIsValid = email.value.trim() !== '' && email.value.includes('@');
+	const messageIsValid = message.value.trim() !== '';
 
-  const nameIsInvalid = !nameIsValid && name.blurred;
-  const emailIsInvalid = !emailIsValid && email.blurred;
-  const messageIsInvalid = !messageIsValid && message.blurred;
+	const nameIsInvalid = !nameIsValid && name.blurred;
+	const emailIsInvalid = !emailIsValid && email.blurred;
+	const messageIsInvalid = !messageIsValid && message.blurred;
 
-  useEffect(() => {
-    if (isSubmitting) {
-      console.log('Sending message...');
-      const timer = setTimeout(() => {
-        setIsSubmitting(false);
-      }, 1000);
+	useEffect(() => {
+		if (isSubmitting) {
+			console.log('Sending message...');
+			const timer = setTimeout(() => {
+				setIsSubmitting(false);
+			}, 1000);
 
-      return () => clearTimeout(timer);
-    }
-  }, [isSubmitting]);
+			return () => clearTimeout(timer);
+		}
+	}, [isSubmitting]);
 
-  function changeInputHandler(event) {
-    dispatch({
-      type: 'INPUT_CHANGE',
-      input: event.target.id,
-      value: event.target.value,
-    });
-  }
+	function changeInputHandler(event) {
+		dispatch({
+			type: 'INPUT_CHANGE',
+			input: event.target.id,
+			value: event.target.value,
+		});
+	}
 
-  function blurInputHandler(event) {
-    dispatch({
-      type: 'INPUT_BLUR',
-      input: event.target.id,
-    });
-  }
+	function blurInputHandler(event) {
+		dispatch({
+			type: 'INPUT_BLUR',
+			input: event.target.id,
+		});
+	}
 
-  function submitHandler(event) {
-    event.preventDefault();
+	function submitHandler(event) {
+		event.preventDefault();
 
-    if (!nameIsValid || !emailIsValid || !messageIsValid) {
-      return;
-    }
+		if (!nameIsValid || !emailIsValid || !messageIsValid) {
+			return;
+		}
 
-    setIsSubmitting(true);
-  }
+		setIsSubmitting(true);
+	}
 
-  return (
-    <>
-      <h2 className="center">Contact Us</h2>
-      <form className={classes.form} onSubmit={submitHandler}>
-        <p className={messageIsInvalid ? classes.invalid : undefined}>
-          <label htmlFor="message">Your Message</label>
-          <textarea
-            id="message"
-            rows="5"
-            onChange={changeInputHandler}
-            onBlur={blurInputHandler}
-            required
-            data-cy="contact-input-message"
-          />
-        </p>
-        <div className={classes.row}>
-          <p className={nameIsInvalid ? classes.invalid : undefined}>
-            <label htmlFor="name">Your Name</label>
-            <input
-              type="text"
-              id="name"
-              onChange={changeInputHandler}
-              onBlur={blurInputHandler}
-              required
-              data-cy="contact-input-name"
-            />
-          </p>
-          <p className={emailIsInvalid ? classes.invalid : undefined}>
-            <label htmlFor="email">Your Email</label>
-            <input
-              type="email"
-              id="email"
-              onChange={changeInputHandler}
-              onBlur={blurInputHandler}
-              required
-              data-cy="contact-input-email"
-            />
-          </p>
-        </div>
-        <p className={classes.actions}>
-          <button
-            type="submit"
-            data-cy="contact-btn-submit"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? 'Sending...' : 'Send Message'}
-          </button>
-        </p>
-      </form>
-    </>
-  );
+	return (
+		<>
+			<h2 className="center">Contact Us</h2>
+			<form className={classes.form} onSubmit={submitHandler}>
+				<p className={messageIsInvalid ? classes.invalid : undefined}>
+					<label htmlFor="message">Your Message</label>
+					<textarea
+						id="message"
+						rows="5"
+						onChange={changeInputHandler}
+						onBlur={blurInputHandler}
+						required
+						data-cy="contact-input-message"
+					/>
+				</p>
+				<div className={classes.row}>
+					<p className={nameIsInvalid ? classes.invalid : undefined}>
+						<label htmlFor="name">Your Name</label>
+						<input
+							type="text"
+							id="name"
+							onChange={changeInputHandler}
+							onBlur={blurInputHandler}
+							required
+							data-cy="contact-input-name"
+						/>
+					</p>
+					<p className={emailIsInvalid ? classes.invalid : undefined}>
+						<label htmlFor="email">Your Email</label>
+						<input
+							type="email"
+							id="email"
+							onChange={changeInputHandler}
+							onBlur={blurInputHandler}
+							required
+							data-cy="contact-input-email"
+						/>
+					</p>
+				</div>
+				<p className={classes.actions}>
+					<button
+						type="submit"
+						data-cy="contact-btn-submit"
+						disabled={isSubmitting}
+					>
+						{isSubmitting ? 'Sending...' : 'Send Message'}
+					</button>
+				</p>
+			</form>
+		</>
+	);
 }
 
 export default ContactForm;
