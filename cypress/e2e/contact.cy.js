2eece799cc9a60e47d984d0a92a67a0727851ef9
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-12-24 22:25:50 +0100

    added tests

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -16,21 +16,26 @@ describe('contact form', () => {
 		cy.visit('http://localhost:5173/about');
 		cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
 		cy.get('@submitBtn').click();
+		cy.screenshot();
 		cy.get('@submitBtn').then((el) => {
 			expect(el).not.to.have.attr('disabled');
 			expect(el).not.to.equal('Sending...');
 		});
+		cy.screenshot();
+
 		cy.get('[data-cy="contact-input-message"]').blur();
 		cy.get('[data-cy="contact-input-message"]')
 			.parent()
 			.should('have.attr', 'class')
 			.and('match', /invalid/);
+		cy.screenshot();
 
 		cy.get('[data-cy="contact-input-name"]').focus().blur();
 		cy.get('[data-cy="contact-input-name"]')
 			.parent()
 			.should('have.attr', 'class')
 			.and('match', /invalid/);
+		cy.screenshot();
 
 		cy.get('[data-cy="contact-input-email"]').focus().blur();
 		cy.get('[data-cy="contact-input-email"]')
